fa528b00d8e2a5164bed5686614c64d0cea237ba
Neeraj Doharey <neerajlivewire@gmail.com>
2017-11-02 21:02:16 +0530

    Sum of even number

--- fibbonaci.rb
+++ fibbonaci.rb
@@ -1,18 +1,17 @@
 def fibbonaci
-	previous_num = 2
-	current_num = 4
-	print " #{previous_num}, #{current_num}"
+	previous_num = 1
+	current_num, even_sum = 2, 2
 	while true
 		sum_num = previous_num + current_num
 		previous_num = current_num
 		current_num = sum_num
-		if current_num < 4000000
-			print ", #{sum_num}"
-		else	
-			puts
-			return
+		if current_num <= 4000000
+			even_sum  += current_num if current_num % 2 == 0 
+		else
+			return even_sum
 		end
 	end
 end
 
-fibbonaci()
+result = fibbonaci()
+puts "Result : #{result}"
